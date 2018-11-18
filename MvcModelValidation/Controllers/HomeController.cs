96f1484967b28e3d688ae9d9181f840b75c3edac
Jon Perkins <captainperk81@gmail.com>
2018-11-18 08:11:31 -0500

    Creating the Controller

---
+++ MvcModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcModelValidation.Models;
+using System;
+
+namespace MvcModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("MakeBooking", new Appointment{ Date = DateTime.Now});
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appointment) => View("Completed", appointment);
+    }
+}
