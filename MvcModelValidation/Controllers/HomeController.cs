732923b9d0ae5e7ee0303dd547a6c7ee8db7ec3f
Jon Perkins <captainperk81@gmail.com>
2018-11-18 08:45:37 -0500

    Adding Model Validation to the Controller

--- MvcModelValidation/Controllers/HomeController.cs
+++ MvcModelValidation/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using MvcModelValidation.Models;
 using System;
 
@@ -9,6 +10,29 @@ namespace MvcModelValidation.Controllers
         public IActionResult Index() => View("MakeBooking", new Appointment{ Date = DateTime.Now});
 
         [HttpPost]
-        public ViewResult MakeBooking(Appointment appointment) => View("Completed", appointment);
+        public ViewResult MakeBooking(Appointment appointment)
+        {
+            if (string.IsNullOrEmpty(appointment.ClientName))
+            {
+                ModelState.AddModelError(nameof(appointment.ClientName), "Please enter your name");
+            }
+
+            if (ModelState.GetValidationState("Date") == ModelValidationState.Valid && DateTime.Now > appointment.Date)
+            {
+                ModelState.AddModelError(nameof(appointment.Date), "Please enter a date in the future");
+            }
+
+            if (!appointment.TermsAccepted)
+            {
+                ModelState.AddModelError(nameof(appointment.TermsAccepted), "You must accept the terms");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            return View("Completed", appointment);
+        }
     }
 }
